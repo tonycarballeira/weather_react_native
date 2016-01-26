f9c91df17801bb5d337f2a580901bda0059aee87
Tony Carballeira <tonycarballeira@gmail.com>
2016-01-26 16:54:17 -0500

    Required React library along with MapView component

---
+++ index.ios.js
@@ -0,0 +1,21 @@
+var React = require('react-native');
+
+var {
+  AppRegistry,
+  MapView,
+  View,
+  Stylesheet
+} = React;
+
+
+var Weather = React.createClass({
+  render: function() {
+
+  }
+});
+
+var styles = StyleSheet.create({
+  map: {
+    flex: 1
+  }
+});
