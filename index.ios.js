a5a800f924b648a2355d6140b9ce0adb4a5b8df9
Tony Carballeira <tonycarballeira@gmail.com>
2016-01-26 17:51:23 -0500

    Added onRegionChangeComplete attribute to MapView

--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,5 @@
+// IMPORT LIBRARIES 
+
 var React = require('react-native');
 
 var {
@@ -7,21 +9,32 @@ var {
   StyleSheet
 } = React;
 
+// CREATE COMPONENT
 
 var Weather = React.createClass({
 
   render: function() {
-    return <MapView style={styles.map}>
+    return <MapView 
+            style={styles.map}
+            onRegionChangeComplete={this.onRegionChangeComplete}>
 
            </MapView>
+  },
+
+  onRegionChangeComplete: function(region) {
+    console.log(region);
   }
-  
+
 });
+
+// STYLING
 
 var styles = StyleSheet.create({
   map: {
     flex: 1
   }
 });
+
+// REGISTER COMPONENT
 
 AppRegistry.registerComponent('weather', () => Weather);
