40a3164c661fd39281a3b60020a31dec5c389e18
Tony Carballeira <tonycarballeira@gmail.com>
2016-01-27 12:07:35 -0500

    Parsed weather description

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 var rootUrl = 'api.openweathermap.org/data/2.5/weather?APPID=d08317194587af3ae0544bc9983a6afa'
 
 var kelvinToF = function(kelvin) {
-	return Math.round((kelvin - 283.15) * 1.8 + 32)
+	return Math.round((kelvin - 273.15) * 1.8 + 32) + ' ˚F'
 }
 
 module.exports = function(latitude, longitude) {
@@ -9,15 +9,15 @@ module.exports = function(latitude, longitude) {
 
 	fetch(url)  // this will make request to the api server then come back and preforn the function below
 		.then(function(response){
-			return response.json() 
+			return response.json() // formats what was received by request into a json promise
 		})
-		.then(function(json){
+		.then(function(json){  //  parse promise for actual usable json data
 			return {
 				city: json.city,  // city closest to provided latitude and longitude
 				temperature: kelvinToF(json.main.temp),
-				description:
+				description: json.weather[0].description
 			}
-		})
+		});
 }
 
 
