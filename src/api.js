4918f979b16d811bc8c0b6e88172859ba6f74d88
Tony Carballeira <tonycarballeira@gmail.com>
2016-01-27 12:01:22 -0500

    Implemented fetch call to weather api created kelvin conversion function

--- src/api.js
+++ src/api.js
@@ -1,7 +1,23 @@
 var rootUrl = 'api.openweathermap.org/data/2.5/weather?APPID=d08317194587af3ae0544bc9983a6afa'
 
+var kelvinToF = function(kelvin) {
+	return Math.round((kelvin - 283.15) * 1.8 + 32)
+}
+
 module.exports = function(latitude, longitude) {
 	var url = `${rootUrl}&lat=${latitude}&lon=${longitude}`;  //  this is using template strings
+
+	fetch(url)  // this will make request to the api server then come back and preforn the function below
+		.then(function(response){
+			return response.json() 
+		})
+		.then(function(json){
+			return {
+				city: json.city,  // city closest to provided latitude and longitude
+				temperature: kelvinToF(json.main.temp),
+				description:
+			}
+		})
 }
 
 
